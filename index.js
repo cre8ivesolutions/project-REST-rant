84c2cdc11fd2104e867d646fbcb576b411311ad2
cre8ivesolutions <tammerz78@gmail.com>
2022-07-27 21:04:58 -0500

    Part Six: Pass Some Mock Data

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
+
+  })
+  
 
 app.get('/', (req, res) => {
   res.send('Hello world home page')
@@ -15,11 +35,9 @@ app.get('/', (req, res) => {
     res.render('home')
   })
 
-
   app.get('*', (req, res) => {
     res.render('error404')
   })
   
 
 app.listen(process.env.PORT)
-
