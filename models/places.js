a6337a2f0a86c6c801c032d7b50d9fda376c6205
cre8ivesolutions <tammerz78@gmail.com>
2022-07-28 22:15:12 -0500

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Nitty Gritty',
+    city: 'Sun Prairie',
+    state: 'WI',
+    cuisines: 'American, Traditional',
+    pic: '/images/nittygritty.jpg',
+  }, {
+    name: 'Buck and Honeys',
+    city: 'Madison',
+    state: 'WI',
+    cuisines: 'American, Traditional',
+    pic: '/images/bucknhoneys.jpg',
+  }]
