6b6a3293645e1516665cc861555799e2650c9c73
cre8ivesolutions <tammerz78@gmail.com>
2022-07-30 08:17:43 -0500

    finished part 5

--- controllers/places.js
+++ controllers/places.js
@@ -12,16 +12,15 @@ router.get('/', (req, res) => {
 
 
 router.post('/', (req, res) => {
-    console.log(req.body)
     if (!req.body.pic) {
       // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
+      req.body.pic = '/images/goodfood.jpg'
     }
     if (!req.body.city) {
-      req.body.city = 'Anytown'
+      req.body.city = 'Somewhere'
     }
     if (!req.body.state) {
-      req.body.state = 'USA'
+      req.body.state = 'WI, USA'
     }
     places.push(req.body)
     res.redirect('/places')
--- models/places.js
+++ models/places.js
@@ -3,11 +3,14 @@ module.exports = [{
     city: 'Sun Prairie',
     state: 'WI',
     cuisines: 'American, Traditional',
-    pic: '/images/nittygritty.jpg',
+    pic: '/images/Nitty-gritty.jpg',
+    // Photo by <a href="https://unsplash.com/@mab_studio?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michele Blackwell</a> on <a href="https://unsplash.com/s/photos/restaurant-food-square?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
   }, {
     name: 'Buck and Honeys',
     city: 'Madison',
     state: 'WI',
     cuisines: 'American, Traditional',
-    pic: '/images/bucknhoneys.jpg',
+    pic: '/images/buckandhoneys.jpg'
+    // Photo by <a href="https://unsplash.com/@ferhadd?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Farhad Ibrahimzade</a> on <a href="https://unsplash.com/s/photos/american-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   }]
