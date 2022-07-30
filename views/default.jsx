a04895628860bf425dbe34fd8ae46df74f0f2d7e
cre8ivesolutions <tammerz78@gmail.com>
2022-07-30 08:29:18 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,15 +9,15 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
-            <nav className="navbar navbar-light bg-light justify-content-between">
+            <nav 
+            // className="navbar navbar-light bg-light justify-content-between"
+            >
             <a className ="navbar-brand"></a>
-            <form className="form-inline">
+            {/* <form className="form-inline">
                 <input className ="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 </input>
                 <button className ="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-</nav>
-            <nav>
+            </form> */}
                 <ul>
                     <li>
                     <a href="/">Home</a>
