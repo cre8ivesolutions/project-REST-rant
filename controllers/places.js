75f3847c424aab58d85865665c71f08c3b0c41f1
cre8ivesolutions <tammerz78@gmail.com>
2022-08-06 10:08:21 -0500

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -65,6 +65,35 @@ router.post('/', (req, res) => {
       res.redirect('/places')
     }
   })
+
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'public\images\goodfood.jpg'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+  
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+  
+  
   
 
 module.exports = router
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -28,7 +28,7 @@ function edit_form (data) {
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input className="btn btn-primary" type="submit" value="Edit Place" />
                 </div>
             </form>
           </main>
