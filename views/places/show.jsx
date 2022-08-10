3e477126d18a9413ef5cfcb97f47ba866438a5fb
cre8ivesolutions <tammerz78@gmail.com>
2022-08-09 21:40:55 -0500

    show

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,9 +5,26 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <h2>Currently unrated</h2>
-            <p>No comments yet</p>
+<div className="row">
+      <div className="col-sm-6">
+          <img className="showImg" src={data.place.pic} alt={data.place.name} />
+          <h3>
+            Located in {data.place.city}, {data.place.state}
+          </h3>
+      </div>
+      <div className="col-sm-6">
+        <h1>{ data.place.name }</h1>
+        <h3>Currently unrated</h3>
+        <h2>Rating </h2>
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>     
@@ -16,10 +33,17 @@ function show (data) {
                 Delete
               </button>
             </form> 
-          </main>
-        </Def>
+        </div>
+        <h2>Comments</h2>
+            <p>No comments yet</p>
+     </div>
+    </main>
+   </Def>
     )
 }
+
+ 
+
 
 module.exports = show
 
